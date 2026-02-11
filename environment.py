1db98ebc1724e93d9e930167211fd96d5ca5aad8
doggone28 <doggone28.gamer@gmail.com>
2026-02-11 16:03:19 -0500

    betterGrid

--- environment.py
+++ environment.py
@@ -5,6 +5,8 @@
 
 import numpy as np
 import random
+from typing import Tuple
+
 
 #*Our silly little robot world
 #*We can model our world with a 2D array
@@ -23,7 +25,69 @@ class GridWorld:
         self.start = None
         self.end = None
 
+    def generateMaze(self, blockProb):
+
+        visited = np.zeroes((self.size, self.size), dtype = bool)
+        stack = []
+
+        startXPos, startYPos = random.randint(0, self.size - 1), random.randint(0, self.size - 1)
+        visited[startXPos, startYPos] = True
+        self.grid[startXPos, startYPos] = 0
+        stack.append((startXPos, startYPos))
+
+
+        while not np.all(visited):
+            if stack:
+                current = stack[-1]
+                unvisitedNeighbors = self.unvisitedNeighbors(current, visited)
+                if unvisitedNeighbors:
+                    neighbor = random.choice(unvisitedNeighbors)
+                    nx, ny = neighbor  
+
+                    visited[nx, ny] = True
+
+                    if random.random() < blockProb:
+                        self.grid[nx, ny] = 1
+                    else:
+                        self.grid[nx, ny] = 0
+                        stack.append(neighbor)
+
+                else:
+                    stack.pop()
+
+            else:
+                unvisited = np.argwhere(~visited)
+                if len(unvisited) > 0:
+                    nextCell = tuple(unvisited[random.randint(0, len(unvisited) - 1)])
+                    visited[nextCell] = True
+                    self.grid[nextCell] = 0
+                    stack.append(nextCell)
+                else:
+                    break
+
+    def unvisitedNeighbors(self, cell: Tuple[int,int], visited: np.ndarray) -> list:
+        x, y = cell
+        neighbors = []
+
+        directions = [
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1)
+        ]
+
+        for dx, dy in directions:
+            nx = x + dx
+            ny = y + dy
+
+            if (0 <= nx < self.size and 0 <= ny < self.size and not visited[nx, ny]):
+                neighbors.append((nx, ny))
+
+        return neighbors
+
+
 
     
+
 
     
---
+++ util.py
@@ -0,0 +1,5 @@
+
+
+#!special binary heap for keepign track of the a* open list
+
+class BinaryHeap:
